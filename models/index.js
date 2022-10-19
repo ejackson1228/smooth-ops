735c7715bb350914c4ca48de11114f43b50cbdfb
ejackson1228 <ejackson1228@gmail.com>
2022-10-19 14:24:45 -0400

    established relationships across tables

---
+++ models/index.js
@@ -0,0 +1,76 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Vote = require('./Vote');
+const Pokemon = require('./Pokemon');
+
+// User relationships
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id',
+});
+
+//Post relationships
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade'
+});
+
+Post.hasOne(Pokemon, {  //post has one poke-team
+    foreignKey: 'post_id',
+    onDelete: 'cascade'
+});
+
+//Comment relationshps
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Vote relationships
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//Pokemon relationships
+Pokemon.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
