ee20864c694e6fd78039a265a8aa85ee5bcb72c4
ejackson1228 <ejackson1228@gmail.com>
2022-10-25 19:15:40 -0400

    FINALLY FIXED THE MODELS FOR GOOD.. i think.

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -5,7 +5,7 @@ class Pokemon extends Model {}
 
 Pokemon.init(
     {
-        pokemon_id: {
+        id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
--- models/Team.js
+++ models/Team.js
@@ -18,6 +18,13 @@ Team.init(
                 model: 'user',
                 key: 'id'
             }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
         }
         // post_id: {
         //     type: DataTypes.INTEGER,
