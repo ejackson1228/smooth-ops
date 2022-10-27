191846fb159a2112d61e2d0d4b4c7440f5083d89
ejackson1228 <ejackson1228@gmail.com>
2022-10-27 14:50:21 -0400

    added helpers.js and bones of pokeball selection dropdown, it still needs to be tested/debugged.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
+const { format } = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,8 +22,8 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
