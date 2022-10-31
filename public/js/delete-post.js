1b71e970f1539076536ad00d5a4921e13081a86f
ejackson1228 <ejackson1228@gmail.com>
2022-10-31 18:00:38 -0400

    fixed delete

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -1,30 +1,19 @@
-async function commentFormHandler(event) {
+async function deletePostHandler(event) {
     event.preventDefault();
 
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-
     const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
+        window.location.toString().split('/').length -1 
     ];
+    
+    let response = await fetch(`/api/posts/${post_id}`, {
+        method: 'delete'
+    });
 
-    if (comment_text) {
-        const response = await fetch('/api/comments', {
-            method: 'POST',
-            body: JSON.stringify({
-                post_id,
-                comment_text
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
     }
-}
+};
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('.delete-post-btn').addEventListener('click', deletePostHandler);
